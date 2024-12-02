f89d3525584c545484efd0b4615d3e7207d2bab9
Xeeshan <xeeshann.ahsan@gmail.com>
2024-12-03 01:31:58 +0600

    some fixation

--- app/categories/recipe/[title]/page.js
+++ app/categories/recipe/[title]/page.js
@@ -106,7 +106,7 @@ const page = ({ params: { title } }) => {
 
         <h2 className="text-3xl font-bold mb-4">Here are the basics</h2>
         <p className="mb-8">
-          Juicy meatballs brisket slammin' baked shoulder. Juicy smoker soy
+          Juicy meatballs brisket slammin&apos; baked shoulder. Juicy smoker soy
           sauce burgers brisket. polenta mustard hunk greens. Wine technique
           snack skewers chuck excess. Oil heat slowly. slices natural delicious,
           set aside magic tbsp skillet, bay leaves brown centerpiece. fruit
@@ -115,11 +115,11 @@ const page = ({ params: { title } }) => {
         </p>
 
         <blockquote className="text-3xl font-bold italic text-center my-12 px-4">
-          "One cannot think well, love well, sleep well, if one has not dined
-          well."
+          &quot;One cannot think well, love well, sleep well, if one has not
+          dined well.&quot;
         </blockquote>
         <p className="text-center text-gray-600 mb-12">
-          — Virginia Woolf, A Room of One's Own
+          — Virginia Woolf, A Room of One&apos;s Own
         </p>
 
         <h2 className="text-3xl font-bold mb-4">In the kitchen</h2>
@@ -140,7 +140,7 @@ const page = ({ params: { title } }) => {
         />
 
         <p className="mb-8">
-          Juicy meatballs brisket slammin' baked shoulder. Juicy smoker soy
+          Juicy meatballs brisket slammin&apos; baked shoulder. Juicy smoker soy
           sauce burgers brisket. polenta mustard hunk greens. Wine technique
           snack skewers chuck excess. Oil heat slowly. slices natural delicious,
           set aside magic tbsp skillet, bay leaves brown centerpiece. fruit
